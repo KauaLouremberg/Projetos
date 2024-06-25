8cc20c7e5280fb7b5fef3895111ec20ed3a587ef
KauaLouremberg <161601940+KauaLouremberg@users.noreply.github.com>
2024-06-24 23:59:02 -0300

    Update config.php

--- JN Enterprises/Cadastro/PHP/vendor/includes/config.php
+++ JN Enterprises/Cadastro/PHP/vendor/includes/config.php
@@ -4,6 +4,6 @@ define("SMTP_HOST", "smtp.gmail.com");
 define("SMTP_PORT", "587");
 define("SMTP_USER", "kaualouremberg@gmail.com");
 
-define("SMTP_PASS", "ridp ljza chax sgnq");
+define("SMTP_PASS", "xxxxxxxxxxxxx");
 
 ?>
